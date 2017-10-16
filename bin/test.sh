5423ac5965ceed2cdb4d78575bfa3344a7f7ccc1
Megane <megane.pau@edu.ece.fr>
2017-10-16 12:07:33 +0200

    correction

--- bin/test.sh
+++ bin/test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./node_modules/mocha/bin/mocha $@ --compilers ./node_modules/.bin/coffee:coffee-script --output lib/ src/
+./node_modules/mocha/bin/mocha $@ --compilers ./node_modules/.bin/coffee --output lib/ src/
